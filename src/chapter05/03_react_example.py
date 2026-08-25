e018dee8aaf939ae81d1e9fde865d7c48dd6650c
zhangkun <kun.zhang@wocheng.tv>
2026-08-25 23:37:47 +0800

    Add react example code

---
+++ src/chapter05/03_react_example.py
@@ -0,0 +1,116 @@
+import math
+
+from config.config import API_KEY, BASE_URL, MODEL_NAME
+from langchain_core.messages import HumanMessage, ToolMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, PromptTemplate
+from langchain_openai import ChatOpenAI
+from langgraph.graph import MessagesState, StateGraph, START, END
+
+llm = ChatOpenAI(
+    api_key=API_KEY,
+    base_url=BASE_URL,
+    model=MODEL_NAME
+)
+
+def mocked_google_search(query: str) -> str:
+    print(f"CALLED GOOGLE SEARCH with query={query}")
+    return "Donald Trump is a president of USA and he's 81 years old"
+
+def mocked_calculator(expression: str) -> float:
+    print(f"CALLED CALCULATOR with expression={expression}")
+    if "sqrt" in expression:
+        return math.sqrt(81 * 132)
+    return 81 * 132
+
+calculator_tool = {
+    "type": "function",
+    "function": {
+        "name": "calculator",
+        "description": "Computes mathematical expressions",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "expression": {
+                    "type": "string",
+                    "title": "expression",
+                    "description": "A mathematical expression to be evaluated by a calculator"
+                }
+            },
+            "required": ["expression"]
+        }
+    }
+}
+
+search_tool = {
+    "type": "function",
+    "function": {
+        "name": "google_search",
+        "description": "Returns about common facts, fresh events and news from Google Search engine based on a query",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "query": {
+                    "type": "string",
+                    "title": "search_query",
+                    "description": "Search query to be sent to the search engine"
+                }
+            },
+            "required": ["query"]
+        }
+    }
+}
+
+system_prompt = (
+    "Always use a calculator for mathematical computations, and use Google Search "
+    "for information about common facts, fresh events and news. Do not assume anything, keep in "
+    "mind that things are changing and always "
+    "check yourself with external sources if possible."
+)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", system_prompt),
+    MessagesPlaceholder(variable_name="messages"),
+])
+
+llm_with_tools = prompt | llm.bind_tools([search_tool, calculator_tool])
+
+
+def invoke_llm(state: MessagesState):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+def call_tools(state: MessagesState):
+    last_message = state["messages"][-1]
+    tool_calls = last_message.tool_calls
+
+    new_messages = []
+
+    for tool_call in tool_calls:
+        if tool_call["name"] == "google_search":
+            tool_result = mocked_google_search(**tool_call["args"])
+            new_messages.append(ToolMessage(content=tool_result, tool_call_id=tool_call["id"]))
+        elif tool_call["name"] == "calculator":
+            tool_result = mocked_calculator(**tool_call["args"])
+            new_messages.append(ToolMessage(content=tool_result, tool_call_id=tool_call["id"]))
+        else:
+            raise ValueError(f"Tool {tool_call['name']} is not defined!")
+    return {"messages": new_messages}
+
+def should_run_tools(state: MessagesState):
+    last_message = state["messages"][-1]
+    if last_message.tool_calls:
+        return "call_tools"
+    return END
+
+builder = StateGraph(MessagesState)
+builder.add_node("invoke_llm", invoke_llm)
+builder.add_node("call_tools", call_tools)
+
+builder.add_edge(START, "invoke_llm")
+builder.add_conditional_edges("invoke_llm", should_run_tools)
+builder.add_edge("call_tools", "invoke_llm")
+graph = builder.compile()
+
+question = "What is a square root of the current US president's age multiplied by 132?"
+
+result = graph.invoke({"messages": [HumanMessage(content=question)]})
+print(result["messages"][-1].content)
